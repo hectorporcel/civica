82565f81e88fd3a28eb98483479a0008fea13434
Héctor Porcel <sporcelamazon@gmail.com>
2026-05-04 16:42:26 +0000

    commit 04.05.2026

--- macros/generate_database_name.sql
+++
@@ -1,9 +0,0 @@
-{% macro generate_database_name(custom_database_name, node) -%}
-
-    {%- if custom_database_name is not none -%}
-        {{ custom_database_name | trim }}
-    {%- else -%}
-        {{ target.database | trim }}
-    {%- endif -%}
-
-{%- endmacro %}

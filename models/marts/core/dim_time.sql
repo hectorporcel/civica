5916f2e2ce4a4c1700a3df87a811d6a43de08b82
Héctor Porcel <sporcelamazon@gmail.com>
2026-05-05 16:14:28 +0000

    05.05.2026 tercer commit

--- models/marts/core/dim_time.sql
+++ models/marts/core/dim_time.sql
@@ -1,28 +1,14 @@
-{{
-    config(
-        materialized = 'table'
-    )
-}}
 
 with base_dates as (
 
     {{
         dbt.date_spine(
-            'day',
-            "cast('2020-01-01' as date)",
-            "cast('2030-01-01' as date)"
+            datepart="day",
+            start_date="cast('2020-01-01' as date)",
+            end_date="current_date()"
         )
     }}
 
-),
-
-final as (
-
-    select
-        cast(date_day as date) as date_day
-    from base_dates
-
 )
 
-select *
-from final
+select date_day from base_dates
